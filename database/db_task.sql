44e4168c0334fd993737a39640b0ccbd23bfb59d
ibeteka <ibrahim.tounkara.dev@hotmail.com>
2021-02-10 13:01:25 +0000

    Changed sql script to psql

--- database/db_task.sql
+++ database/db_task.sql
@@ -7,38 +7,31 @@
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_task`
+-- Database: db_task
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task`
+-- Table structure for table task
 --
 
-CREATE TABLE `task` (
-  `task_id` int(11) NOT NULL,
-  `task` varchar(150) NOT NULL,
-  `status` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE task (task_id SERIAL PRIMARY KEY,
+  task varchar(150) NOT NULL,
+  status varchar(150)
+);
 
 --
--- Dumping data for table `task`
+-- Dumping data for table task
 --
 
-INSERT INTO `task` (`task_id`, `task`, `status`) VALUES
+INSERT INTO task (task_id, task, status) VALUES
 (1, 'Check Errors', 'Done'),
 (4, 'Remove Bugs', ''),
 (5, 'Need Improvements', '');
@@ -47,22 +40,7 @@ INSERT INTO `task` (`task_id`, `task`, `status`) VALUES
 -- Indexes for dumped tables
 --
 
---
--- Indexes for table `task`
---
-ALTER TABLE `task`
-  ADD PRIMARY KEY (`task_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `task`
---
-ALTER TABLE `task`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
