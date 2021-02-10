6d072b533ed9763c50f59751cfedd48cf4029f61
ibeteka <ibrahim.tounkara.dev@hotmail.com>
2021-02-10 12:54:36 +0000

    modified soure code with psql library; moved database files out of frontapp

--- frontapp/add_query.php
+++ frontapp/add_query.php
@@ -2,9 +2,9 @@
 	require_once 'conn.php';
 	if(ISSET($_POST['add'])){
 		if($_POST['task'] != ""){
-			$task = $_POST['task'];
-			
-			$conn->query("INSERT INTO `task` VALUES('', '$task', '')");
+			$value['task'] = $_POST['task'];
+			/*$conn->query("INSERT INTO `task` VALUES('', '$task', '')");*/
+			pg_insert($conn, 'task', $value);
 			header('location:index.php');
 		}
 	}
--- frontapp/conn.php
+++ frontapp/conn.php
@@ -1,7 +1,11 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", "db_task");
-	
+	/*$conn = new mysqli("localhost", "root", "", "db_task");
 	if(!$conn){
 		die("Error: Cannot connect to the database");
-	}
+	}*/
+
+	$conn = pg_connect("host=172.17.0.3 port=5432 dbname=db_task user=postgres password=postgres");
+	if(!$conn){
+                die("Error: Cannot connect to the database");
+        }
 ?>
