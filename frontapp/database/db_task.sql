902484dec87c1fce2e683d4502105f5114efaf78
ibeteka <ibrahim.tounkara.dev@hotmail.com>
2021-02-11 15:48:25 +0100

    kube config files, docke config files

--- frontapp/database/db_task.sql
+++ frontapp/database/db_task.sql
@@ -7,38 +7,31 @@
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_task`
+-- Database: db_task
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task`
+-- Table structure for table task
 --
 
-CREATE TABLE `task` (
-  `task_id` int(11) NOT NULL,
-  `task` varchar(150) NOT NULL,
-  `status` varchar(150) NOT NULL
+CREATE TABLE task (task_id int(11) NOT NULL,
+  task varchar(150) NOT NULL,
+  status varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `task`
+-- Dumping data for table task
 --
 
-INSERT INTO `task` (`task_id`, `task`, `status`) VALUES
+INSERT INTO task (task_id, task, status) VALUES
 (1, 'Check Errors', 'Done'),
 (4, 'Remove Bugs', ''),
 (5, 'Need Improvements', '');
@@ -48,20 +41,20 @@ INSERT INTO `task` (`task_id`, `task`, `status`) VALUES
 --
 
 --
--- Indexes for table `task`
+-- Indexes for table task
 --
-ALTER TABLE `task`
-  ADD PRIMARY KEY (`task_id`);
+ALTER TABLE task
+  ADD PRIMARY KEY (task_id);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `task`
+-- AUTO_INCREMENT for table task
 --
-ALTER TABLE `task`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
+ALTER TABLE task
+  MODIFY task_id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
